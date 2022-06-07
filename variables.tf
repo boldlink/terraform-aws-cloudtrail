5f9431f05a094f962ae5a47091c44fb61b7b4d96
patrick <mukumbu.katiwa@boldlink.io>
2022-06-07 15:19:03 +0300

    NOSTORY: Re-modified default log retention to 5 yrs as 7 yrs not supported

--- variables.tf
+++ variables.tf
@@ -120,7 +120,7 @@ variable "external_kms_key_id" {
 variable "log_retention_days" {
   type        = number
   description = "Specifies the number of days you want to retain log events in the specified log group. Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653, and 0. If you select 0, the events in the log group are always retained and never expire."
-  default     = 2557
+  default     = 1827
 }
 
 variable "sns_topic_name" {
